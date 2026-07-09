efa51837e8bf4f860174d086e0ba9a54e0323023
Ismini Skoutari <iskoutari@novibet.com>
2026-07-09 11:01:12 +0300

    Align master with CharalamposTsitsanos reference code

--- WorldRank/IWalletRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿using WorldRank;
-
-public interface IWalletRepository
-{
-    void Add(Wallet wallet, int playerId);
-    List<Wallet> GetByPlayerId(int playerId);
-}
--- WorldRank/_logger.cs
+++
@@ -1,7 +0,0 @@
-﻿using WorldRank;
-
-internal class _logger
-{
-    internal static void Info(string v, decimal balance, Wallet.Currency walletCurrency, bool isBlocked)
-        => throw new NotImplementedException();
-}
